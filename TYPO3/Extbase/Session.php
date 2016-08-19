bdc2c7c4770ec4415ffe1708193a71c244dfea79
Steve Lenz <steve.lenz@xima.de>
2016-08-19 11:22:07 +0200

    [Feature] Changeable session type

--- TYPO3/Extbase/Session.php
+++ TYPO3/Extbase/Session.php
@@ -16,11 +16,12 @@ class Session
      *
      * @param string $key
      * @param mixed $data
+     * @param string $type - possible values: ses|user
      */
-    public static function set($key, $data)
+    public static function set($key, $data, $type = 'ses')
     {
         $sessionData = serialize($data);
-        $GLOBALS['TSFE']->fe_user->setKey('ses', $key, $sessionData);
+        $GLOBALS['TSFE']->fe_user->setKey(self::isType($type), $key, $sessionData);
         $GLOBALS['TSFE']->fe_user->storeSessionData();
     }
 
@@ -28,11 +29,12 @@ class Session
      * Restore data from fe_user session
      *
      * @param string $key
+     * @param string $type - possible values: ses|user
      * @return mixed
      */
-    public static function get($key)
+    public static function get($key, $type = 'ses')
     {
-        $sessionData = $GLOBALS['TSFE']->fe_user->getKey('ses', $key);
+        $sessionData = $GLOBALS['TSFE']->fe_user->getKey(self::isType($type), $key);
 
         return unserialize($sessionData);
     }
@@ -41,21 +43,23 @@ class Session
      * Checks whether a key in fe_user session exists
      *
      * @param string $key
+     * @param string $type - possible values: ses|user
      * @return boolean
      */
-    public static function has($key)
+    public static function has($key, $type = 'ses')
     {
-        return ($GLOBALS['TSFE']->fe_user->getKey('ses', $key)) ? true : false;
+        return ($GLOBALS['TSFE']->fe_user->getKey(self::isType($type), $key)) ? true : false;
     }
 
     /**
      * Removes fe_user session data by key
      *
      * @param string $key
+     * @param string $type - possible values: ses|user
      */
-    public static function remove($key)
+    public static function remove($key, $type = 'ses')
     {
-        $GLOBALS['TSFE']->fe_user->setKey('ses', $key, null);
+        $GLOBALS['TSFE']->fe_user->setKey(self::isType($type), $key, null);
         $GLOBALS['TSFE']->fe_user->storeSessionData();
     }
 
@@ -81,6 +85,20 @@ class Session
     public static function delete()
     {
         $GLOBALS['TSFE']->fe_user->removeSessionData();
+    }
+
+    /**
+     * @param string $type
+     * @return mixed
+     * @throws \Exception
+     */
+    protected static function isType($type)
+    {
+        if (false == in_array($type, array('ses', 'user'))) {
+            throw new \Exception('Wrong session type "' . $type . '"!');
+        }
+
+        return $type;
     }
 
 }
