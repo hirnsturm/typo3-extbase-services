a7da145042a9755759355754d878c6169e2f766e
Steve Lenz <steve.lenz@xima.de>
2016-09-01 11:59:47 +0200

    [Bugfix] EntityUtility::mergeEntities() - Catch method for property does not exists

--- TYPO3/Extbase/Domain/Utility/EntityUtility.php
+++ TYPO3/Extbase/Domain/Utility/EntityUtility.php
@@ -92,21 +92,24 @@ class EntityUtility
         $properties = $reflect->getProperties(\ReflectionProperty::IS_PUBLIC | \ReflectionProperty::IS_PROTECTED);
 
         foreach ($properties as $property) {
-            $value = $mergeEntity->{'get' . ucfirst($property->getName())}();
+
+            if (!method_exists($mergeEntity, 'get' . ucfirst($property->getName()))) {
+                continue;
+            }
 
             if (!empty($value)) {
                 if (is_a($value, '\\TYPO3\\CMS\\Extbase\\Persistence\\ObjectStorage')) {
-                    $targetEntity->{'set' . ucfirst($property)}(new ObjectStorage());
+                    $targetEntity->{'set' . ucfirst($property->getName())}(new ObjectStorage());
                     if (is_array($value)) {
-                        $singular = (preg_match('~s$~i', $property) > 0)
-                            ? rtrim($property, 's')
-                            : sprintf('%ss', $property);
+                        $singular = (preg_match('~s$~i', $property->getName()) > 0)
+                            ? rtrim($property->getName(), 's')
+                            : sprintf('%ss', $property->getName());
                         foreach ($value->toArray() as $item) {
                             $targetEntity->{'add' . ucfirst($singular)}($item);
                         }
                     }
                 } else {
-                    $targetEntity->{'set' . ucfirst($property)}($value);
+                    $targetEntity->{'set' . ucfirst($property->getName())}($value);
                 }
             }
         }
